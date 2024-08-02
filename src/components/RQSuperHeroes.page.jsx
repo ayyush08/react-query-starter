2bbe4a358bd0e3708bb63fe606f99e1a2f0caaf6
ayyush08 <ayushkumargupta2908@gmail.com>
2024-08-02 13:42:26 +0530

    chore: useQuery on click implementation

--- src/components/RQSuperHeroes.page.jsx
+++ src/components/RQSuperHeroes.page.jsx
@@ -7,16 +7,17 @@ const fetchSuperHeroes = ()=>{
     return axios.get('http://localhost:4000/superheroes')
 }
 const RQSuperHeroes = () => {
-    const {isLoading,data,isError,error,isFetching } = useQuery('super-heroes',fetchSuperHeroes,
+    const {isLoading,data,isError,error,isFetching,refetch } = useQuery('super-heroes',fetchSuperHeroes,
         {
             // staleTime: 5000,
             // refetchInterval:2000,
             // refetchIntervalInBackground:true
+            enabled:false
         }
     )
 
     console.log({isLoading,isFetching});
-    if(isLoading){
+    if(isLoading || isFetching){
         return <h2>Loading...</h2>
     }
     if(isError){
@@ -25,6 +26,7 @@ const RQSuperHeroes = () => {
     return (
         <>
         <h2>RQ Super Heroes Page</h2>
+        <button onClick={refetch}>Fetch Heroes</button>
         {data?.data.map(hero=>{
             return <div key={hero.name}>{hero.name}</div>
         })}
