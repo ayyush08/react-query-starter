213e1aa98efcb10112eb9ff8fdb1f7a15ad2fa26
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2024-08-05 08:41:37 +0530

    feat: Add parallel queries for super heroes and friends

--- src/components/ParallelQueries.page.jsx
+++ src/components/ParallelQueries.page.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import { useQuery } from 'react-query'
+import axios from 'axios'
+
+const fetchSuperHeroes = ()=>{
+    return axios.get('http://localhost:4000/superheroes')
+}
+
+const fetchFriends = ()=>{
+    return axios.get('http://localhost:4000/friends')
+}
+
 
 const ParallelQueries = () => {
+
+    const { data:superHeroes } = useQuery('super-heroes', fetchSuperHeroes)
+    const { data:friends }  = useQuery('friends', fetchFriends)
     return (
         <div>
-            ParallelQueries
+            {
+                superHeroes && friends && (
+                    <div>
+                        <h2>Super Heroes</h2>
+                        <ul>
+                            {
+                                superHeroes.data.map(hero=>(
+                                    <li key={hero.id}>{hero.name}</li>
+                                ))
+                            }
+                        </ul>
+                        <h2>Friends</h2>
+                        <ul>
+                            {
+                                friends.data.map(friend=>(
+                                    <li key={friend.id}>{friend.name}</li>
+                                ))
+                            }
+                        </ul>
+                    </div>
+            )}
         </div>
     )
 }
